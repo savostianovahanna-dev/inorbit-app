7e2cc2d7c368b8ff055daf6922c35c1a6bc00ba0
savostianovahanna-dev <savostianova.hanna@gmail.com>
2026-05-04 16:44:06 +0300

    CI: install Flutter and fix config generation

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
+
+# Install Flutter
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
+
+flutter precache --ios
 flutter pub get
-flutter build ios --config-only
-cd ios
+
+# Generate Flutter build files
+flutter build ios --config-only --release --no-codesign
+
+# Fix Pods deployment targets
+cd $CI_PRIMARY_REPOSITORY_PATH/ios
 pod install
